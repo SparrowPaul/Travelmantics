862afa3d68b30afa6d008d866a64530ab1be05b2
SparrowPaul <sparrowpaul2@gmail.com>
2019-08-05 16:36:52 +0000

    Initial commit

---
+++ app/src/main/java/com/sparrowpaul/travelmantics/DealActivity.java
@@ -0,0 +1,14 @@
+package com.sparrowpaul.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deal);
+    }
+}
